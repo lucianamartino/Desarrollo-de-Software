04c5ad21df99cd36319926e258a5d6b582a0d54a
EmiRiosUTN <emirioslp@gmail.com>
2024-10-02 16:08:10 -0300

    .env

--- Grupo 9 Desarrollo de SW/src/config.js
+++ Grupo 9 Desarrollo de SW/src/config.js
@@ -5,6 +5,6 @@ config()
 export const PORT = process.env.PORT || 3000
 export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'josefina22'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '1402'
 export const DB_DATABASE = process.env.DB_DATABASE || 'mydb'
 export const DB_PORT = process.env.DB_PORT || 3306
