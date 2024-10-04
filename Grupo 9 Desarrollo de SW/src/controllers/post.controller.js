c63530469308358c99161d9c0694b097c3f1c00a
Bautista Flores <bautista22flores@gmial.com>
2024-10-04 19:08:59 -0300

    cambios

--- Grupo 9 Desarrollo de SW/src/controllers/post.controller.js
+++ Grupo 9 Desarrollo de SW/src/controllers/post.controller.js
@@ -3,7 +3,7 @@ import {pool} from '../db.js'
 // Crea un nuevo post
 export const createPost = async (req, res) => {
     const { descripcion, oficioId } = req.body;
-    const usuarioId = 2
+    const usuarioId = 1
     const valoracion = 5
     const foto = req.file ? req.file.filename : null; // Obtén el nombre del archivo subido
 
